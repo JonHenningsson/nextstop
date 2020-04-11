1d46d8d425961df53449a3ac82b830ba58208de3
Jon Henningsson <jon.henningsson@gmail.com>
2020-04-11 14:46:48 -0500

    Get coordinates from browser

--- static/js/script.js
+++ static/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(async function() {
 
   var nearest_stops_m1_g = {};
   var unique_nums_g = {};
+  var rel_stops_g = {};
 
-  var lat_q1 = getParameterByName("lat1", window.location);
-  var lon_q1 = getParameterByName("lon1", window.location);
-  var lat_q2 = getParameterByName("lat2", window.location);
-  var lon_q2 = getParameterByName("lon2", window.location);
+  var lat1_q = getParameterByName("lat1", window.location);
+  var lon1_q = getParameterByName("lon1", window.location);
+  var lat2_q = getParameterByName("lat2", window.location);
+  var lon2_q = getParameterByName("lon2", window.location);
 
-
-// FUNCTION: getParameterByName - Get query params
+  // FUNCTION: getParameterByName - Get query params
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -19,38 +19,61 @@ $(document).ready(async function() {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
-// END FUNCTION: getParameterByName
+  // END FUNCTION: getParameterByName
 
-async function reCaptchaSuccessful(){
-  $("#search-btn").prop("disabled", false);
-  $("#google-recaptcha").hide();
-}
+  // FUNCTION: getCoords - get coordinates from browser and execute callback
+  async function getCoords(myCallback) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
 
-async function verifyreCaptcha(recaptcha_response) {
-  let recaptchav2_response = grecaptcha.getResponse();
-
-  if (recaptchav2_response) {
-
-    const result = await $.ajax({
-      url: "/.netlify/functions/recaptchaV2-validate",
-      type: "GET",
-      data: {
-        "response": recaptchav2_response,
-      },
-      dataType: "json"
-    });
-
-    if (result.success == true) {
-      reCaptchaSuccessful();
+        myCallback(lat, lon);
+      });
+    } else {
+      alert("Sorry, your browser does not support HTML5 geolocation.");
     }
 
+    return;
   }
+  // END FUNCTION: getCoords
 
-  return;
 
-}
+  // FUNCTION: reCaptchaSuccessful
+  async function reCaptchaSuccessful() {
+    $("#search-btn").prop("disabled", false);
+  }
+  // END FUNCTION: reCaptchaSuccessful
 
-// FUNCTION: sortByDistance - Return array sorted by distance to stop
+  // FUNCTION: verifyreCaptcha - Validate recaptcha server-side
+
+  async function verifyreCaptcha(recaptcha_response) {
+    let recaptchav2_response = grecaptcha.getResponse();
+
+    if (recaptchav2_response) {
+
+      const result = await $.ajax({
+        url: "/.netlify/functions/recaptchaV2-validate",
+        type: "GET",
+        data: {
+          "response": recaptchav2_response,
+        },
+        dataType: "json"
+      });
+
+      if (result.success == true) {
+        reCaptchaSuccessful();
+      }
+
+    }
+
+    return;
+
+  }
+  // END FUNCTION: verifyreCaptcha
+  window.verifyreCaptcha = verifyreCaptcha;
+
+  // FUNCTION: sortByDistance - Return array sorted by distance to stop
   async function sortByDistance(stops) {
     let stops_sorted = [];
     $.each(stops, async function(i) {
@@ -89,18 +112,18 @@ async function verifyreCaptcha(recaptcha_response) {
   }
   // END FUNCTION: compareStops
 
-// FUNCTION: findRelevantStops - Return array of stops relevant to the transport number, sorted by distance to stop
+  // FUNCTION: findRelevantStops - Return array of stops relevant to the transport number, sorted by distance to stop
   async function findRelevantStops(stops, nums, num_sel, direction_sel) {
 
     let direction = nums[num_sel].directions[direction_sel];
     let possible_stops = {};
 
-    for(var key in stops) {
+    for (var key in stops) {
       let stopid = key;
       $.each(stops[stopid].departures, function(k) {
         let dep_direction = stops[stopid].departures[k].direction;
         let dep_num = stops[stopid].departures[k].transportNumber;
-        if ( (dep_direction == direction) && (dep_num == num_sel)  ) {
+        if ((dep_direction == direction) && (dep_num == num_sel)) {
           possible_stops[stopid] = stops[stopid];
         }
       });
@@ -145,7 +168,7 @@ async function verifyreCaptcha(recaptcha_response) {
   }
   // END FUNCTION: timetable
 
- // FUNCTION: findUniqueNums - Return table of transport numbers and associated directions
+  // FUNCTION: findUniqueNums - Return table of transport numbers and associated directions
   async function findUniqueNums(stops) {
     let nums = {};
 
@@ -177,14 +200,18 @@ async function verifyreCaptcha(recaptcha_response) {
     let stops = {};
     let nearby_stops_res = await nearbyStops(lat, lon, false, false);
 
-    for(i = 0; i < nearby_stops_res.StopLocation.length; i++) {
+    if (!nearby_stops_res.StopLocation) {
+      return false;
+    }
+
+    for (i = 0; i < nearby_stops_res.StopLocation.length; i++) {
       let stopid = nearby_stops_res.StopLocation[i].id;
       stops[stopid] = nearby_stops_res.StopLocation[i];
     }
 
     // get departures for each stop
     if (getTT) {
-      for(var key in stops) {
+      for (var key in stops) {
         let stopid = key;
         let timetable_res = await timetable(stopid);
         stops[stopid]["departures"] = timetable_res["Departure"];
@@ -199,56 +226,49 @@ async function verifyreCaptcha(recaptcha_response) {
   }
   // END FUNCTION: findNearestStops
 
-  $("#search-btn").click(async function() {
-    $("#searchresult ol").html("<p>Loading..</p>");
-    let lat1, lon1;
+  // FUNCTION: firstSearch - perform initial search and render html result
+  async function firstSearch(lat, lon) {
+    lat1 = lat1_q || lat;
+    lon1 = lon1_q || lon;
+    console.log("Coordinates:");
 
-    if (lat_q1 && lon_q1) {
-      lat1 = lat_q1;
-      lon1 = lon_q1;
-    } else {
-      console.log("Get coordinates..");
-    }
+    console.log(lat1);
+    console.log(lon1);
 
     nearest_stops_m1_g = await findNearestStops(lat1, lon1, true, false);
+
+    if (nearest_stops_m1_g == false) {
+      $("#searchresult p").html("Hittade ingen hållplats nära dig.");
+      return;
+    }
     unique_nums_g = await findUniqueNums(nearest_stops_m1_g);
 
     $("#searchresult ol").html("");
-    $.each(unique_nums_g, async function(key, value){
+    $("#searchresult p").html("");
+    console.log(unique_nums_g);
+    $.each(unique_nums_g, async function(key, value) {
       let transportNumber = key;
 
-      $.each(unique_nums_g[transportNumber].directions, async function(key, value){
+      $.each(unique_nums_g[transportNumber].directions, async function(key, value) {
         let directionIndex = key;
         let direction = unique_nums_g[transportNumber].directions[directionIndex];
         let linkindex = transportNumber + "-" + directionIndex;
-        $("#searchresult ol").append("<li><a id= '" + linkindex + "' href='#" + linkindex + "'>Transport no " + transportNumber + " to " + direction + "</a></li>");
+        $("#searchresult ol").append("<li><a id= '" + linkindex + "' href='#" + linkindex + "'>Nummer " + transportNumber + " mot " + direction + "</a></li>");
       });
-
     });
-  });
+  }
+  // END FUNCTION: firstSearch
 
-  $("#searchresult").on("click", "ol li a", async function(event) {
-    let href_arr = $(this).attr("href").replace("#", "").split("-");
-    let rel_stops = await findRelevantStops(
-                                nearest_stops_m1_g,
-                                unique_nums_g,
-                                href_arr[0],
-                                href_arr[1]
-                          );
+  // FUNCTION: secondSearch - perform second search and render html result (next stop)
+  async function secondSearch(lat, lon) {
 
-    let lat2, lon2;
-    if (lat_q2 && lon_q2) {
-      lat2 = lat_q2;
-      lon2 = lon_q2;
-    } else {
-      // wait a few seconds to obtain new position..
-      console.log("Get coordinates..");
-    }
+    lat2 = lat2_q || lat;
+    lon2 = lon2_q || lon;
 
     let nearest_stops_m2 = {};
     nearest_stops_m2 = await findNearestStops(lat2, lon2, false, true);
 
-    let compare_stops_res = await compareStops(rel_stops, nearest_stops_m2);
+    let compare_stops_res = await compareStops(rel_stops_g, nearest_stops_m2);
 
     let ns = [];
     let ps = [];
@@ -262,8 +282,38 @@ async function verifyreCaptcha(recaptcha_response) {
       }
     });
 
-    console.log("Next stop: " + ns[0].name + " , distance (meters): " + ns[0].dist);
-    console.log("Past stop: " + ps[0].name + " , distance (meters): " + ps[0].dist);
+    let next_stop_res = "Nästa hållplats: " + ns[0].name + ". Distans: " + ns[0].dist + "m";
+    let past_stop_res = "Föregående hållplats: " + ps[0].name + ". Distans: " + ps[0].dist + "m";
+
+    $("#searchresult p").html("");
+    $("#searchresult ol").html("");
+
+    $("#next-stop-result p").html(next_stop_res);
+    $("#past-stop-result p").html(past_stop_res);
+
+  }
+
+  // END FUNCTION: secondSearch
+
+  $("#search-btn").click(async function() {
+    $("#search-btn").hide();
+    $("#google-recaptcha").hide();
+    $("#searchresult p").html("Vänta, söker..");
+    getCoords(firstSearch);
+  });
+
+  $("#searchresult").on("click", "ol li a", async function(event) {
+    let href_arr = $(this).attr("href").replace("#", "").split("-");
+    rel_stops_g = await findRelevantStops(
+      nearest_stops_m1_g,
+      unique_nums_g,
+      href_arr[0],
+      href_arr[1]
+    );
+
+    setTimeout(function() {
+      getCoords(secondSearch)
+    }, 1000);
 
   });
 
